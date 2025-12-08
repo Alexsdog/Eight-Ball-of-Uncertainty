ff17e028d84b3dd1f8d63841c3d7ad3255ef0a7a
CalvinGupta <calverton4@gmail.com>
2025-12-08 08:47:11 -0500

    Fixed index.html

---
+++ main.js
@@ -0,0 +1,14 @@
+function review1() {
+    document.getElementById("reviewer").src = "Assets/Reviewer1.jpg";
+    document.getElementById("review").innerHTML = "“Every time I asked it a question, it basically shrugged at me. Honestly impressive commitment to not helping.” - Sarah H."
+}
+
+function review2() {
+    document.getElementById("reviewer").src = "Assets/Reviewer2.jpg";
+    document.getElementById("review").innerHTML = "“It’s like consulting a wise oracle who's really into being vague. 10/10 entertainment value.” - Dylan G."
+}
+
+function review3() {
+    document.getElementById("reviewer").src = "Assets/Reviewer3.jpg";
+    document.getElementById("review").innerHTML = "“Not helpful, but somehow still comforting—like being confused, but professionally.” - Erik H."
+}
